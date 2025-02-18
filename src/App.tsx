7cfd61f5720ee2febcd3b18cd2a233ad5e568051
tiago-espadinha <tiago.espadinha.simoes@gmail.com>
2025-02-18 17:43:00 +0000

    chore: initial project structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Gym Tracker</h1>
+      <p>Growing into a modular workout tracker...</p>
+    </main>
+  )
+}
+
+export default App
